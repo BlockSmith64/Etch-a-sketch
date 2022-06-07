03d4fb1cb456abcb7de9efd93e32f0def02ed1e3
Kabilla <a.hadooke3141@gmail.com>
2022-06-07 13:41:57 +0300

    added black color button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+"use strict";
 const content = document.querySelector(".content");
 const container = document.querySelector(".container");
-const btnsContainer = document.querySelector(".btns");
+const btns = document.querySelector(".btns");
 
 //a function that creates any amount of divs based on given arguement value
 const board = function (rows, columns) {
@@ -9,8 +10,24 @@ const board = function (rows, columns) {
     container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     container.appendChild(divs).classList.add("pixels");
-    divs.style.cssText = "border: 1px solid black";
   }
 };
 
 board(16, 16);
+
+const blackBtn = document.createElement("button");
+const pixels = document.querySelectorAll(".pixels");
+
+const blackColor = function () {
+  blackBtn.textContent = "BLACK";
+  blackBtn.addEventListener("click", function () {
+    pixels.forEach((pixel) =>
+      pixel.addEventListener("mouseover", function () {
+        pixel.style.cssText = "background: #000;";
+      })
+    );
+  });
+  btns.appendChild(blackBtn);
+};
+
+blackColor();
