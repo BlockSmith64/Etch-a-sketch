046ff053701375311f943c6bee2cb6febe464fb5
Kabilla <a.hadooke3141@gmail.com>
2022-06-09 23:58:13 +0300

    refactored code

--- js/script.js
+++ js/script.js
@@ -4,65 +4,70 @@ const container = document.querySelector(".container");
 const btns = document.querySelector(".btns");
 
 //a function that creates any amount of divs based on given arguement value
-const board = function (rows, columns) {
-  for (let i = 0; i < rows * columns; i++) {
+const board = function (size) {
+  for (let i = 0; i < size * size; i++) {
     const divs = document.createElement("div");
-    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-    container.appendChild(divs).classList.add("pixels");
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.appendChild(divs).classList.add("pixel");
+    divs.style.cssText = "border: 1px solid pink;";
   }
 };
 
-board(16, 16);
+board(16);
 
-const blackBtn = document.createElement("button");
-const pixels = document.querySelectorAll(".pixels");
+const pixels = document.querySelectorAll(".pixel");
 
-const blackColor = function () {
-  blackBtn.textContent = "BLACK";
-  blackBtn.addEventListener("click", function () {
+const colorPicker = function (color) {
+  if (color === "black") {
     pixels.forEach((pixel) =>
       pixel.addEventListener("mouseover", function () {
         pixel.style.cssText = "background: #000;";
       })
     );
+  } else if (color === "rainbow") {
+    console.log("rgb");
+    pixels.forEach((pixel) =>
+      pixel.addEventListener("mouseover", function () {
+        let randomNum1 = Math.floor(Math.random() * 256);
+        let randomNum2 = Math.floor(Math.random() * 256);
+        let randomNum3 = Math.floor(Math.random() * 256);
+        pixel.style.cssText = `background: rgb(${randomNum1}, ${randomNum2}, ${randomNum3});`;
+      })
+    );
+  } else if (color === "erase") {
+    console.log("erase");
+    pixels.forEach((pixel) =>
+      pixel.addEventListener("mouseover", function () {
+        pixel.style.cssText = "background: #FFFFF;";
+      })
+    );
+  }
+};
+
+const blackColor = function () {
+  const blackBtn = document.querySelector(".btn-black");
+  blackBtn.addEventListener("click", function () {
+    colorPicker("black");
   });
-  btns.appendChild(blackBtn);
 };
 
 blackColor();
 
-const rgbBtn = document.createElement("button");
-
-const rgbColor = function () {
-  rgbBtn.textContent = "RGB";
-  rgbBtn.addEventListener("click", function () {
-    pixels.forEach((pixel) =>
-      pixel.addEventListener("mouseover", function () {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
-        pixel.style.cssText = `background: rgb(${r}, ${g}, ${b});`;
-      })
-    );
+const rainbow = function () {
+  const rainbowBtn = document.querySelector(".btn-rainbow");
+  rainbowBtn.addEventListener("click", function () {
+    colorPicker("rainbow");
   });
-  btns.appendChild(rgbBtn);
 };
 
-rgbColor();
-
-const eraseBtn = document.createElement("button");
+rainbow();
 
 const erase = function () {
-  eraseBtn.textContent = "ERASE";
+  const eraseBtn = document.querySelector(".btn-erasor");
   eraseBtn.addEventListener("click", function () {
-    pixels.forEach((pixel) =>
-      pixel.addEventListener("mouseover", function () {
-        pixel.style.cssText = "background: #FFFFFF;";
-      })
-    );
+    colorPicker("erase");
   });
-  btns.appendChild(eraseBtn);
 };
 
 erase();
